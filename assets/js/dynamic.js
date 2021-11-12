ce2c0f181ef96ed23fc31627c1cd10001b0248de
ijun17 <ijun17@naver.com>
2021-11-12 21:59:46 +0900

    dynamic.js

--- assets/js/dynamic.js
+++ assets/js/dynamic.js
@@ -5,37 +5,14 @@ xmlHttp.onreadystatechange = function () {
     }
 };
 
-
 function getPost(url){
     xmlHttp.open("GET", url, true);
     xmlHttp.send();
 }
 
 function setPost(text){
-    /*extract info from post layout
-    <div class="post">
-        <p class="post-dir" ... > ... </p>
-        <h1 class="post-title" ... > ... </h1>
-        <p class="post-date" ... > ... </p>
-        <div class="post-line"></div>
-        <div class="post-content"> ... </div>
-        <div class="post-line"></div>
-    </div>
-    */
-    const postContent=document.querySelector(".post-content");
-    const postDate=document.querySelector(".post-date");
-    const postTitle=document.querySelector(".post-title");
-    const postDir=document.querySelector(".post-dir");
-
-    text.replace(/.*<div class="post">(.*)<\/div>.*/s,function(match,p1){return p1}) //remove not-post div 
-    text.replace(/<div class="post-line"><\/div>/g,"") //remove line
-
-    text.replace(/<div class="post-content">(.*)<\/div>/s,function(match,p1){postContent.innerHTML=p1;return "";})//get content
-    text.replace(/<p class="post-date"[^>]*>(.*)<\/p>/,function(match,p1){postDate.innerText=p1;return "";})//get date
-    text.replace(/<h1 class="post-title"[^>]*>(.*)<\/h1>/,function(match,p1){postTitle.innerText=p1;return "";})//get title
-    text.replace(/<p class="post-dir"[^>]*>(.*)<\/p>/,function(match,p1){postDir.innerText=p1;return "";})//get dir
-
-    history.pushState(null, null, postDir.innerText);
+    const post=document.querySelector(".post");
+    text.replace(/.*<div class="post">(.*)<\/div>.*/s,function(match,p1){post.innerHTML=p1;}) //extract post from text parameter
     setDynamicEvent();
 }
 
@@ -43,6 +20,7 @@ function setDynamicEvent() {
     let dynamicPostList = document.querySelectorAll(".dynamic-post");
     for (let i=0,l=dynamicPostList.length; i<l; i++) {
         dynamicPostList[i].addEventListener("click", function () {
+            history.pushState(null, null, dynamicPostList[i].dataset.url);
             getPost(dynamicPostList[i].dataset.url);
         })
     }
@@ -50,5 +28,5 @@ function setDynamicEvent() {
 setDynamicEvent();
 
 window.onpopstate = function(e){
-    getPost(document.location)
+    getPost(document.location);
 };
